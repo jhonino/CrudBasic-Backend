afee180445194fcc57d66e74f2f60adfd4fa3616
unknown <PJudicial@DESKTOP-I2PTM12.csjar.pj.gob.pe>
2025-04-25 16:37:05 -0500

    implementando listar, guardar y delete

---
+++ src/main/java/pe/dhexsoft/CrudBasic/service/CustomerServiceImpl.java
@@ -0,0 +1,45 @@
+package pe.dhexsoft.CrudBasic.service;
+
+import org.springframework.stereotype.Service;
+import pe.dhexsoft.CrudBasic.entity.Customer;
+import pe.dhexsoft.CrudBasic.repository.CustomerRepository;
+
+import java.util.List;
+
+@Service
+public class CustomerServiceImpl implements CustomerService{
+
+    //constructor con inyeccion de dependencia
+    private final CustomerRepository customerService;
+
+    public CustomerServiceImpl(CustomerRepository customerService) {
+        this.customerService = customerService;
+    }
+
+
+    @Override
+    public Customer guardar(Customer customer) {
+        return customerService.save(customer);
+    }
+
+    @Override
+    public List<Customer> Listar() {
+        return customerService.findAll();
+    }
+
+    @Override
+    public Customer ListarXId(Long id) {
+        // ala espera
+        return null;
+    }
+
+    @Override
+    public Customer modificar(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public void eliminar(Long id) {
+        customerService.deleteById(id);
+    }
+}
