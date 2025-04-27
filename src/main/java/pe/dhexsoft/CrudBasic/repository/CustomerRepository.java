163e2fbb91c535e5dc4e8ccb780de531a6a39702
jhonino <jinofuenteap@gmail.com>
2025-04-27 11:41:51 -0500

    corrigiendo el repository

--- src/main/java/pe/dhexsoft/CrudBasic/repository/CustomerRepository.java
+++ src/main/java/pe/dhexsoft/CrudBasic/repository/CustomerRepository.java
@@ -1,7 +1,8 @@
 package pe.dhexsoft.CrudBasic.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pe.dhexsoft.CrudBasic.entity.Customer;
-
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
